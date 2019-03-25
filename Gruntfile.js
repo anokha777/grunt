7a32f09b0c55e281a86fef549949b37f6a016e99
brajesh.anokha <brajesh.anokha@sapient.com>
2019-03-25 11:22:02 +0530

    WIP

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,44 @@
+const path = require('path');
+
 module.exports = function(grunt) {
-    require('jit-grunt')(grunt)({
-        // customTasksDir: 'grunt-task',
-        jitGrunt: true
-    });
-    require('time-grunt')(grunt);
-    
-    grunt.initConfig({
-        cssmin:{
-            my_target: {
-                files: [{
-                    expand: true,
-                    cwd: './css',
-                    src: ['*.css', '!*.min.css'],
-                    dest: './css',
-                    ext: '.min.css'
-                }]
+    require('jit-grunt')(grunt, grunt.log.writeln(path.join(process.cwd())))({
+        
+        jitGrunt: true,
+        customTasksDir: 'grunt-task',
+        // configPath: path.join(process.cwd(), 'grunt-task'),
+        // init: true, 
+        jitGrunt: {
+            staticMappings: {
+                cssmin: 'grunt-task/cssmin.js',
             }
         }
     });
 
-    grunt.registerTask('default', ['cssmin']);
+    // require('load-grunt-config')(grunt, {
+    //     jitGrunt: {
+    //         // here you can pass options to jit-grunt (or just jitGrunt: true)
+    //         staticMappings: {
+    //             // here you can specify static mappings, for example:
+    //             cssmin: 'grunt-task/cssmin.js'
+    //         }
+    //     }
+    // });
+
+    require('time-grunt')(grunt);
+    
+    // grunt.initConfig({
+        // cssmin:{
+        //     my_target: {
+        //         files: [{
+        //             expand: true,
+        //             cwd: './css',
+        //             src: ['*.css', '!*.min.css'],
+        //             dest: './css',
+        //             ext: '.min.css'
+        //         }]
+        //     }
+        // }
+    // });
+
+   grunt.registerTask('default', ['cssmin']);
 };
--- grunt-task/cssmin.js
+++ grunt-task/cssmin.js
@@ -1,11 +1,22 @@
-module.exports = {
-    my_target: {
-        files: [{
-            expand: true,
-            cwd: './css',
-            src: ['*.css', '!*.min.css'],
-            dest: './css',
-            ext: '.min.css'
-        }]
-    }
+module.exports = function (grunt) {
+    grunt.log.writeln('This is from cssmin.js');
+    // grunt.initConfig({
+    //     cssmin:{
+    //         my_target: {
+    //             files: [{
+    //                 expand: true,
+    //                 src: ['css/*.*'],
+    //                 }]
+    //         }
+    //     }
+    // });
+    // // my_target: {
+    //     files: [{
+    //         expand: true,
+    //         cwd: 'css',
+    //         src: ['*.css'],
+    //         dest: '/tmp',
+    //         ext: '.min.css'
+    //     }]
+    // }
 };
